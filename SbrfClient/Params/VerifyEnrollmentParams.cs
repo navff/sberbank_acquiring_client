876fce00023309a9c1e64edfb19f4c17fb44b6df
navff <var@33kita.ru>
2018-01-18 07:23:57 +0300

    комменты к классам

---
+++ SbrfClient/Params/VerifyEnrollmentParams.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SbrfClient.Params
+{
+    /// <summary>
+    /// Для проверки вовлечённости карты в 3DS используется запрос verifyEnrollment.
+    /// </summary>
+    public class VerifyEnrollmentParams
+    {
+        /// <summary>
+        /// Номер карты
+        /// </summary>
+        public string pan { get; set; }
+    }
+}
--- SbrfClient/Response/GetOrderStatusExtendedResponse.cs
+++ SbrfClient/Response/GetOrderStatusExtendedResponse.cs
@@ -8,10 +8,31 @@ namespace SbrfClient.Response
 {
     public class GetOrderStatusExtendedResponse
     {
+        /// <summary>
+        /// Номер (идентификатор) заказа в системе магазина.
+        /// </summary>
         public string orderNumber { get; set; }
+
+        /// <summary>
+        /// По значению этого параметра определяется состояние заказа в
+        /// платёжной системе. Отсутствует, если заказ не был найден
+        /// </summary>
         public int orderStatus { get; set; }
+
+        /// <summary>
+        /// Код ответа
+        /// </summary>
         public int actionCode { get; set; }
+
+        /// <summary>
+        /// Расшифровка кода ответа на языке, переданном в параметре Language
+        /// в запросе
+        /// </summary>
         public string actionCodeDescription { get; set; }
+
+        /// <summary>
+        /// Код ошибки. Возможны следующие варианты
+        /// </summary>
         public int errorCode { get; set; }
         public string errorMessage { get; set; }
         public long amount { get; set; }
@@ -67,5 +88,43 @@ namespace SbrfClient.Response
         public string bankName { get; set; }
         public string bankCountryCode { get; set; }
         public string bankCountryName { get; set; }
+    }
+
+    public enum OrderStatus
+    {
+        /// <summary>
+        /// Заказ зарегистрирован, но не оплачен;
+        /// </summary>
+        Registered = 0,
+
+        /// <summary>
+        /// Предавторизованная сумма захолдирована (для двухстадийных
+        /// платежей);        /// </summary>
+        Holded = 1,
+
+        /// <summary>
+        /// Проведена полная авторизация суммы заказа;
+        /// </summary>
+        FullAuthorized = 2,
+
+        /// <summary>
+        /// Авторизация отменена;
+        /// </summary>
+        AuthorizationCancelled = 3,
+
+        /// <summary>
+        /// По транзакции была проведена операция возврата;
+        /// </summary>
+        Refunded = 4,
+
+        /// <summary>
+        ///  Инициирована авторизация через ACS банка-эмитента;
+        /// </summary>
+        InClientACSProgress = 5,
+
+        /// <summary>
+        /// Авторизация отклонена.
+        /// </summary>
+        AuthorizationDeclined = 6
     }
 }
