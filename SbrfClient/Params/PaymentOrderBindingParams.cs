943286b259828e9dc231bb47799af9e0b21c7481
Vladimir Varnavskiy <var@33kita.ru>
2019-04-04 17:46:50 +0300

    Начал собирать запрос paymentOrderBinding.do

---
+++ SbrfClient/Params/PaymentOrderBindingParams.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SbrfClient.Params
+{
+    public class PaymentOrderBindingParams
+    {
+        public string MdOrder { get; set; }
+
+        public string BindingId { get; set; }
+
+        public int Language { get; set; }
+
+        public string Ip { get; set; }
+
+        public int Cvc { get; set; }
+
+        public string Email { get; set; }
+    }
+
+}
---
+++ SbrfClient/Requests/PaymentOrderBindingRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SbrfClient.Params;
+
+namespace SbrfClient.Requests
+{
+    internal class PaymentOrderBindingRequest : PaymentOrderBindingParams
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+
+        public PaymentOrderBindingRequest(PaymentOrderBindingParams paymentOrderBindingParams)
+        {
+            this.BindingId = paymentOrderBindingParams.BindingId;
+            this.Cvc = paymentOrderBindingParams.Cvc;
+            this.Email = paymentOrderBindingParams.Email;
+            this.Ip = paymentOrderBindingParams.Ip;
+            this.Language = paymentOrderBindingParams.Language;
+            this.MdOrder = paymentOrderBindingParams.MdOrder;
+        }
+    }
+}
